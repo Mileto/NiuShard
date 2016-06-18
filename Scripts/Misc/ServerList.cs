fcc6513e6f637aac77aa0b81f871254853c24fce
giga487 <giga487@hotmail.com>
2016-06-18 12:23:27 +0200

    Cambiato il nome del server

--- Scripts/Misc/ServerList.cs
+++ Scripts/Misc/ServerList.cs
@@ -51,7 +51,7 @@ namespace Server.Misc
 
 		public static readonly bool AutoDetect = Config.Get("Server.AutoDetect", true);
 
-		public static string ServerName = Config.Get("Server.Name", "My Shard");
+        public static string ServerName = "Storm Finocchio"; // Config.Get("Server.Name", "My Shard");
 
 		private static IPAddress _PublicAddress;
 
